89bfad4c536b0d9f72856c991255658c60a07a73
Andy Kakkaramadam <andy.kakkaramadam@gmail.com>
2013-10-05 18:44:43 -0400

    Adding the DropBoxManager Wrapper Files
    Files from eclipse.

---
+++ src/DropBoxManager/src/dropboxmanager/DropBoxFolder.java
@@ -0,0 +1,65 @@
+
+package dropboxmanager;
+import java.util.List;
+import java.util.ArrayList;
+public class DropBoxFolder extends DropBoxDataType {
+	
+	List<DropBoxDataType> links_to;
+	List<DropBoxFolder> sub_folders;
+	
+	public DropBoxFolder()
+	{
+		super();
+		links_to = new ArrayList<DropBoxDataType>();
+		sub_folders = new ArrayList<DropBoxFolder>();
+	}
+	
+	public DropBoxFolder(char new_name, List<DropBoxDataType> contains)
+	{
+		super();
+		name = new_name;
+		links_to = contains;
+		
+	}
+	
+	public DropBoxFolder(char new_name, int new_size, List<DropBoxDataType> contains)
+	{ 
+		super(new_name, new_size);
+		links_to = contains;
+	}
+	
+	public DropBoxFolder(char new_name, int new_size)
+	{ 
+		super(new_name, new_size);
+	}
+	
+	public List<DropBoxDataType> Get_List_Of_Files()
+	{
+		return links_to;
+	}
+	
+	public void Add_To_List_Of_Files(DropBoxDataType new_file)
+	{
+		links_to.add(new_file);
+	}
+	
+	public void change_name_of_folder(char new_name)
+	{
+		name = new_name;
+	}
+	
+	protected int determine_size()
+	{
+		size = 0;
+		for(DropBoxDataType d : links_to)
+		{
+			if(!(d instanceof DropBoxFolder)) size+=d.size;
+			else if(d instanceof DropBoxFolder)
+			{
+				//DropBoxFolder p = d;
+				size+=((DropBoxFolder)(d)).determine_size();
+			}
+		}	
+		return size;
+	}
+}
---
+++ src/DropBoxManager/src/dropboxmanager/DropBoxManager.java
@@ -0,0 +1,10 @@
+package dropboxmanager;
+
+public class DropBoxManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
